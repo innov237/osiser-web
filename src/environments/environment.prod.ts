3991bdd18d0209c59e031ae1a38463c1443ddc71
charleswona <Charles.mwemo@naumur.com>
2021-01-09 11:41:29 +0100

    get init pull for befor change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'http://127.0.0.1:8000/'
+  // api_url: 'http://127.0.0.1:8000/'
   // api_url: 'http://192.168.43.151:8000/'
-  // api_url:'http://osiserapi.innov237.com/public/'
+  api_url:'http://osiserapi.innov237.com/public/'
 };
